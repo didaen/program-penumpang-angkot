3dea1984b97ba511ef8b83495b76a2f7d02a12dd
Dida En <den.elfsuju@gmail.com>
2021-01-14 15:41:27 +0700

    Berhasil memasukkan undifined ke dalam cobaArray

--- script.js
+++ script.js
@@ -9,21 +9,39 @@ const tambahPenumpang = (namaPenumpang, penumpang) => {
         return penumpang;
     }
 
-    console.log(penumpang);
-    
     // else
-    //     telusuri seluruh kursi dari awal
-    //         jika ada kursi kosong
+
+
+
+    // else if (penumpang.length !== 0) {
+        
+    //     // telusuri seluruh kursi dari awal
+    //     const temukanUndefinedPertama = penumpang.find( (undifinedPertama) => {
+    //         return undifinedPertama === undefined;
+    //     });
+
+    //     return penumpang
+
+        // jika ada kursi kosong
     //             tambah penumpang di kursi tersebut
     //             kembalikan isi array & keluar dari function
+
+    // }
     //         jika sudah ada nama yang sama
     //             tampilkan pesan kesalahannya
     //             kembalikan isi array & keluar dari function
     //         jika seluruh kursi terisi
     //             tambah penumpang di akhir Array
     //             kembalikan isi array & keluar dari function
-
+    
 }
+
+const cobaArray = [];
+cobaArray[0] = "Dida";
+cobaArray[1] = "Isna";
+cobaArray[2] = undefined;
+cobaArray[3] = "Fina";
+console.log(cobaArray);
 
 const greet = () => {
     return "Hello, World";
